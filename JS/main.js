a32d8fc4db8d761b34870c944010f4ec926c03a8
Federico Resca <federesca@gmail.com>
2021-10-01 15:22:08 -0300

    Proyecto final

--- JS/main.js
+++ JS/main.js
@@ -1,57 +1,5 @@
 // Variables
-const ingredientes = [
-    {
-        id:1,
-        name : "Masa",
-        img: '../Assests/ingredientes/MASA.png'
-    },
-    {
-        id:2,
-        name : "Salsa",
-        img : '../Assests/ingredientes/salsa.png'
-    },
-    {
-        id:3,
-        name : "Cebolla Caramelizada",
-        img : '../Assests/ingredientes/cebolla.png'
-    },
-    {
-        id:4,
-        name : "Boconccinos",
-        img : '../Assests/ingredientes/bocconcinos.png'
-    },
-    {
-        id:5,
-        name : "Muzzarela",
-        img : '../Assests/ingredientes/muzza.png'
-    },
-    {
-        id:6,
-        name : "Tomates Secos",
-        img : '../Assests/ingredientes/tomates-secos.png'
-    },
-    {
-        id:7,
-        name : "Panceta",
-        img : '../Assests/ingredientes/panceta.png'
-    },
-    {
-        id:8,
-        name : "Queso Azul",
-        img : '../Assests/ingredientes/roque.png'
-    },
-    {
-        id:9,
-        name : "Huevo",
-        img : '../Assests/ingredientes/huevo.png'
-    },
-    {
-        id:10,
-        name : "Provolone",
-        img : '../Assests/ingredientes/provolone.png'
-    },
-]
-
+let ingredientes = [];
 let ingredientesFavoritos = [];
 
 const contenedorIngredientes = document.querySelector(".contenedor-ingredientes");
@@ -60,9 +8,50 @@ const contenedorFavoritos = document.querySelector(".listado-favoritos");
 obtenerDatosDeLocalStorage();
 
 document.addEventListener('DOMContentLoaded', () => {
+    fetch('/mocks/ingredientes.json')
+        .then((data) => data.json())
+        .then((data) => {
+            ingredientes = data;
+            mostrarIngredientes();
+        });
 
-    mostrarIngredientes();
-})
+    let count = 0;
+//Boton agregar clickeado
+$('.cart-btn').on('click', function (){
+  let cart = $('.cart-nav');
+  // Encontrar imagen clickeada
+  let imgtodrag = $(this).parent('.buttons').parent('.content').parent('.card').find("img").eq(0);
+  if (imgtodrag) {
+    // Duplicar imagen
+    const imgclone = imgtodrag.clone().offset({
+      top: imgtodrag.offset().top,
+      left: imgtodrag.offset().left
+    }).css({
+      'opacity': '0.8',
+      'position': 'absolute',
+      'height': '150px',
+      'width': '150px',
+      'z-index': '100'
+    }).appendTo($('body')).animate({
+      'top': cart.offset().top + 20,
+      'left': cart.offset().left + 30,
+      'width': 75,
+      'height': 75
+    }, 1000, 'easeInOutExpo');
+    setTimeout(function(){
+      count++;
+      $(".cart-nav .item-count").text(count);
+    }, 1500);
+    imgclone.animate({
+      'width': 0,
+      'height': 0
+    }, function(){
+      $(this).detach()
+    });
+  }
+});
+
+});
 
 function mostrarIngredientes(){
     ingredientes.forEach((ingredientes) => {
@@ -105,10 +94,17 @@ function agregarAFavorito(id) {
     localStorage.setItem("ingredientesFavoritos", JSON.stringify(ingredientesFavoritos));
 
     console.log(ingredientesFavoritos);
+
     mostrarIngredientesFavoritos(ingredientesFavoritos);
+    completarCampoIngredientesFavoritos(ingredientesFavoritos);
 }
 
-function mostrarIngredientesFavoritos(arreglo){
+function completarCampoIngredientesFavoritos(arreglo) {
+    const input = document.getElementById('ingredientes_cliente');
+    input.value = arreglo.map(({ name }) => name).join(', ');
+}
+
+function mostrarIngredientesFavoritos(arreglo) {
 
     limpiarHTML();
     arreglo.forEach( ingrediente => {
@@ -136,41 +132,6 @@ function obtenerDatosDeLocalStorage() {
         ingredientesFavoritos = array;
 
         mostrarIngredientesFavoritos(array);
+        completarCampoIngredientesFavoritos(array);
     }
 }
-
-let count = 0;
-//Boton agregar clickeado
-$('.cart-btn').on('click', function (){
-  let cart = $('.cart-nav');
-  // Encontrar imagen clickeada
-  let imgtodrag = $(this).parent('.buttons').parent('.content').parent('.card').find("img").eq(0);
-  if (imgtodrag) {
-    // Duplicar imagen
-    const imgclone = imgtodrag.clone().offset({
-      top: imgtodrag.offset().top,
-      left: imgtodrag.offset().left
-    }).css({
-      'opacity': '0.8',
-      'position': 'absolute',
-      'height': '150px',
-      'width': '150px',
-      'z-index': '100'
-    }).appendTo($('body')).animate({
-      'top': cart.offset().top + 20,
-      'left': cart.offset().left + 30,
-      'width': 75,
-      'height': 75
-    }, 1000, 'easeInOutExpo');
-    setTimeout(function(){
-      count++;
-      $(".cart-nav .item-count").text(count);
-    }, 1500);
-    imgclone.animate({
-      'width': 0,
-      'height': 0
-    }, function(){
-      $(this).detach()
-    });
-  }
-});
